6fff1135e41523165f579886aeac2e3b2d150d86
SathwikaReddyAnumandla <2311cs010714@mallareddyuniversity.ac.in>
2025-01-02 12:24:21 +0530

    for loop 1 to 100

---
+++ demo4.java
@@ -0,0 +1,14 @@
+
+public class demo4{
+    public static void main(String args[]) {
+      
+        // for loop to print numbers from 1 to 5
+        for (int i = 1; i <= 100; i++) {
+          
+            System.out.println("" + i);
+        }
+        
+        System.out.println("Loop has ended.");
+    }
+}
+
