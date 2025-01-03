66730ed4bd55b8744e4671a2aac9520098f59eab
SathwikaReddyAnumandla <2311cs010714@mallareddyuniversity.ac.in>
2025-01-03 10:10:55 +0530

    name and password using while

---
+++ demo6.java
@@ -0,0 +1,12 @@
+public class demo6 {
+    public static void main(String[] args) {
+        String username= "sathwikareddy";
+        String pwd="sathwikareddy@123";
+        while((username == "sathwikareddy") && (pwd == "sathwikareddy@123"))
+        {
+          System.out.println("Hello sathwikareddy!");
+          break;
+        }
+
+    }
+}
