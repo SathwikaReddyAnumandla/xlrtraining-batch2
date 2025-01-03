95970fec52f7025f2d00fbdb3745941e8bdb0812
SathwikaReddyAnumandla <2311cs010714@mallareddyuniversity.ac.in>
2025-01-03 09:48:46 +0530

    while loop for credentials

---
+++ demo5.java
@@ -0,0 +1,14 @@
+public class demo5 {
+    
+    public static void main(String[] args) {
+        String username= "user_001";
+        String pwd="user_001";
+        while(username == "user_001" && pwd == "user_001")
+        {
+          System.out.println("Hello user!");
+          break;
+        }
+
+    }
+    
+}
