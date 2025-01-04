13efd38a58a8caab312444904e42e9c6dbf01c5e
SathwikaReddyAnumandla <2311cs010714@mallareddyuniversity.ac.in>
2025-01-04 10:42:24 +0530

    details via user input

---
+++ demo7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class demo7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+        String name=sc.nextLine();
+        System.out.println("Enter your Age: ");
+        int age=sc.nextInt();
+        System.out.println("Enter your phone number: ");
+        long phnno=sc.nextLong(); 
+        System.out.println(name+" is your name and "+age+"is your age and "+phnno+"is your phone number");
+    }
+    
+}
