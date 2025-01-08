3b689bb0b0576c8f5d8a2d31fc6eb0a42e96aa38
SathwikaReddyAnumandla <2311cs010714@mallareddyuniversity.ac.in>
2025-01-08 09:49:11 +0530

    sum of a two digit number

---
+++ demo017.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class demo017 {
+    public static void main (String[] args){
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter your number: ");
+       System.out.print("Enter a two-digit number: ");
+       int number = sc.nextInt();
+       
+    
+       int tens = number / 10;  
+       int ones = number % 10; 
+       
+       
+       int sum = tens + ones;
+       
+       
+       System.out.println(tens + " + " + ones + " = " + sum);
+
+    }
+    
+}
