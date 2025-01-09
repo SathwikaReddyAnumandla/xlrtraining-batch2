3b7fdb510d82a3296fd2f627b81dbaa398845ce3
SathwikaReddyAnumandla <2311cs010714@mallareddyuniversity.ac.in>
2025-01-09 22:21:30 +0530

    fibonacci series

---
+++ demo019.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class demo019 {
+    static void fibo(int N){
+        int num1 = 0, num2 = 1;
+
+        for (int i = 0; i < N; i++) {
+            
+            System.out.print(num1 + " ");
+
+            
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+        }
+    
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int N=sc.nextInt();
+        fibo(N);
+    }
+    
+}
