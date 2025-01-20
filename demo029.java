1986bb5bfadd157b4583d8b1c3ca6a65ac12f04b
SathwikaReddyAnumandla <2311cs010714@mallareddyuniversity.ac.in>
2025-01-20 10:23:19 +0530

    ARRAY LIST of names

---
+++ demo029.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class demo029{
+    public static void main(String[] args) {
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("charitha");
+        friends.add("swathi");
+        friends.add("siddartha");
+        friends.add("tanushka");
+        friends.add("navya");
+        friends.add("pravallika");
+        friends.add("ashu");
+        friends.add("sahasra");
+        friends.add("pranitha");
+        friends.add("sushanth");
+
+        System.out.println(friends);
+    }
+}
