e7a84f891576e630958780c7e25e1da756e74fac
SathwikaReddyAnumandla <2311cs010714@mallareddyuniversity.ac.in>
2025-01-27 09:41:50 +0530

    TWO SUM(leet code)

---
+++ LTcode005.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class LTcode005 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0;; ++i) {
+            int x = nums[i];
+            int y = target - x;
+            if (m.containsKey(y)) {
+                return new int[] {m.get(y), i};
+            }
+            m.put(x, i);
+        }
+    }
+
+    public static void main(String[] args) {
+        LTcode005 solution = new LTcode005();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+}
+}
+
